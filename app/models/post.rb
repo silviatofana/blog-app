1b317900df6683f2e99a5d7925477de97e41d803
silviatofana <silvia.tofana@gmail.com>
2022-09-14 20:15:28 +0300

    update post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
-end
+    has_many :comments
+    has_many :likes
+  
+    belongs_to :user
+  
+    def update_post_counter
+      user.increment!(:posts_counter)
+    end
+  
+    def recent_comments
+      comments.limit(5).order(created_at: :desc)
+    end
+  end
+  
