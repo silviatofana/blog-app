efd73fc4234c01e15caebd86713ef2efeb50fd46
silviatofana <silvia.tofana@gmail.com>
2022-09-15 15:49:17 +0300

    posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @user_id = params[:user_id]
+    user_id = params[:user_id]
+    @user = User.includes(:posts).find(user_id)
   end
 
   def show
-    @user_id = params[:user_id]
-    @id = params[:id]
+    user_id = params[:user_id]
+    post_id = params[:id]
+    @user = User.find(user_id)
+    @post = @user.posts.includes(:comments, :likes).find(post_id)
+  end
+
+  def new
+    @current = current_user
+  end
+
+  def create
+    new_post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      format.html do
+        if new_post.save
+          redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post created successfully'
+        else
+          render :new, alert: 'An error occured. Please try again!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
