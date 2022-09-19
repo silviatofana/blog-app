9d54a9559e38aa526b4caaf59f93534b203a15bf
silviatofana <silvia.tofana@gmail.com>
2022-09-19 16:33:13 +0300

    update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  def index; end
 
   def show
-    @user = User.find(params[:id])
+    @id = params[:id]
   end
 end
+
