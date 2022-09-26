11d485cf144a6652e5e4b195fbdc289044aa61d2
silviatofana <silvia.tofana@gmail.com>
2022-09-26 15:56:15 +0300

    update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_comments_counter
-    post.increment!(:comments_counter)
+  def post_update
+    # A method that updates the posts counter for a user.
+    user.increment!(:posts_counter)
   end
 end
