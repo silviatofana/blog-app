2aef2821da43358ae54648a9cd7a1619df0c9417
silviatofana <silvia.tofana@gmail.com>
2022-09-26 16:07:07 +0300

    add posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,36 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
+RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before(:example) { get user_posts_path(1) }
-
-    it 'should be a success' do
-      expect(response).to have_http_status(:ok)
+    before(:example) { get user_path('id') }
+    it 'response status correct' do
+      expect(response).to have_http_status(:success)
     end
-
-    it "should render 'index' template" do
+    it 'renders correct template' do
       expect(response).to render_template('index')
     end
-
-    it 'should return the correct placeholder text' do
-      expect(response.body).to include('Here is a list of posts for user with id:')
+    it 'response body contains correct placeholder text' do
+      expect(response.body).to include('Here is a list of post from this user')
     end
   end
 
   describe 'GET /show' do
-    before(:example) { get user_post_path(1, 2) }
-
-    it 'should be a success' do
-      expect(response).to have_http_status(:ok)
+    before(:example) { get user_post_path(1, 1) }
+    it 'returns response status correct' do
+      expect(response).to have_http_status(:success)
     end
-
-    it "should render 'show' template" do
+    it 'renders correct template' do
       expect(response).to render_template('show')
     end
-
-    it 'should return the correct placeholder text' do
-      expect(response.body).to include('Here is the post with id:')
-      expect(response.body).to include(' from the user with id:')
+    it 'response body contains correct placeholder text' do
+      expect(response.body).to include('Here is the post with the given id')
     end
   end
 end
+
