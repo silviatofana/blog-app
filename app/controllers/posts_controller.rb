3909f9b1ba5a5bae654263ea145fa1a08b4a3074
silviatofana <silvia.tofana@gmail.com>
2022-09-26 15:55:09 +0300

    posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,12 @@
-class PostsController < ApplicationController
+class PostController < ApplicationController
   def index
     user_id = params[:user_id]
-    @user = User.find(user_id)
-    @posts = @user.posts.order(created_at: :desc)
+    @user = User.includes(:posts).find(user_id)
   end
 
   def show
-    user_id = params[:user_id]
-    id = params[:id]
-    @user = User.find(user_id)
-    @post = Post.find(id)
-    @comments = @post.comments.order(created_at: :desc)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:comments, [:user]).find(params[:id])
     @likes = @post.likes.all
   end
 
@@ -24,9 +20,9 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html do
         if new_post.save
-          redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post created successfully'
+          redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post was successfully created.'
         else
-          render :new, alert: 'An error occured. Please try again!'
+          render :new, alert: 'Post was not created.'
         end
       end
     end
