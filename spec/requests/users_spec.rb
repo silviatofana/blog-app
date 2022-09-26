7a864476b2badc2475b52a007fb05519ab3199fc
silviatofana <silvia.tofana@gmail.com>
2022-09-26 16:07:20 +0300

    add users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,34 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before(:example) { get users_path }
+    before(:example) { get user_path }
 
-    it 'should be a success' do
-      expect(response).to have_http_status(:ok)
+    it 'response status correct' do
+      expect(response).to have_http_status(:success)
     end
-
-    it "should render 'index' template" do
+    it 'renders correct template' do
       expect(response).to render_template('index')
     end
-
-    it 'should return the correct placeholder text' do
-      expect(response.body).to include('Here is list of all users and their number of posts.')
+    it 'response body contains correct placeholder text' do
+      expect(response.body).to include('Here is a list of all users and their number of posts')
     end
   end
 
   describe 'GET /show' do
     before(:example) { get user_path(1) }
-
-    it 'should be a success' do
-      expect(response).to have_http_status(:ok)
+    it 'returns response status correct' do
+      expect(response).to have_http_status(:success)
     end
-
-    it "should render 'show' template" do
+    it 'renders correct template' do
       expect(response).to render_template('show')
     end
-
-    it 'should return the correct placeholder text' do
-      expect(response.body).to include('Here is the profile page of user with id:')
+    it 'response body contains correct placeholder text' do
+      expect(response.body).to include('Here is the profile page of user with the given id')
     end
   end
 end
