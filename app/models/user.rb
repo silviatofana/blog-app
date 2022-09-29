bc0e853f77a233dac8b9a8065d94ab547df0e1a4
silviatofana <silvia.tofana@gmail.com>
2022-09-29 14:50:16 +0300

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true, allow_blank: false
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
-  def recent_three_posts
-    posts.order(updated_at: :desc).last(3)
+  ROLES = %i[admin default].freeze
+
+  def is?(requested_role)
+    ROLES == requested_role.to_s
+  end
+
+  # method that returns the 3 most recent posts for a given user
+  def recent_post
+    posts.limit(3).order(created_at: :desc)
   end
 end
