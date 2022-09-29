642ab0f21d92559116195120d9db55a9e12895b6
silviatofana <silvia.tofana@gmail.com>
2022-09-29 12:08:24 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
-  # rubocop:disable Style/SymbolArray
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
 
   devise_scope :user do get '/users/sign_out' => 'devise/sessions#destroy' end
 
+    resources :users, only: [:index, :show]  do
+      resources :posts, only: [:index, :show, :new, :create, :destroy] do
+        resources :comments, only: [:new, :create, :destroy]
+        resources :likes, only: [:create]
+      end
+    end
 
-  resources :comments, only: [:new, :create]
-  resources :likes, only: [:create]
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
-  end
 end
 
 
