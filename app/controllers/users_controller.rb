5af7fafefb2af453d177efdb0137ec923859f4f8
silviatofana <silvia.tofana@gmail.com>
2022-09-29 14:47:33 +0300

    update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find params[:id]
+    @user = User.find(params[:id])
+    @recent_posts = Post.includes(:author, :comments,
+                                  :likes).where(author_id: params[:id]).order(created_at: :desc).limit(3)
   end
 end
