df65556b8dfac28b6326d78839e040b35effca6e
silviatofana <silvia.tofana@gmail.com>
2022-09-29 14:49:33 +0300

    update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,16 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-  after_save :update_comment_counter
+  after_save :update_comments_counter
 
-  private
+  after_destroy :decrease_post_comment_counter
 
-  def update_comment_counter
+  def update_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrease_post_comment_counter
+    post.decrement!(:comments_counter) unless post.comments_counter.zero?
   end
 end
