c3caf9435fc21ea07250909cf7a307d7c99db66c
silviatofana <silvia.tofana@gmail.com>
2022-09-29 12:09:10 +0300

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_133959) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_220534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
+    t.bigint "author_id"
+    t.bigint "post_id", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_133959) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
+    t.bigint "author_id"
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -34,11 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_133959) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
+    t.bigint "author_id"
     t.string "title"
     t.text "text"
-    t.integer "comments_counter", default: 0
-    t.integer "likes_counter", default: 0
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -46,8 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_133959) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
-    t.string "bio"
+    t.string "photo", default: "https://cdn1.iconfinder.com/data/icons/random-115/24/person-512.png"
+    t.text "bio", default: ""
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_133959) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
