4b2d56cafeda3cfc15de43251eca1d3b0a893d7a
silviatofana <silvia.tofana@gmail.com>
2022-09-29 14:59:41 +0300

    update like_spec.rb

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,17 +1,8 @@
 require 'rails_helper'
-
 RSpec.describe Like, type: :model do
-  describe 'Tests for Like model' do
-    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                             bio: 'Teacher from Mexico.')
-    first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-    subject { Like.new }
-    before { subject.save }
-
-    it 'Test if likes_counter is getting updated' do
-      first_post.likes_counter = 2
-      subject.post = first_post
-      expect(subject.post.likes_counter).to eq 2
-    end
+  subject { Like.new }
+  before { subject.save }
+  it 'No post should raise error' do
+    expect { subject.likes_counter }.to raise_error(NoMethodError)
   end
 end
