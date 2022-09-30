64391bdd038f4bffbea42e924c0bde8b4056dc0f
terbeche <termus96@gmail.com>
2022-09-30 08:44:20 +0100

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
-  
   load_and_authorize_resource
   def new
     @comment = Comment.new
   end
+
   def create
     @comment = Comment.new(comment_params)
     redirect_to user_posts_path(id: @comment.post_id, user_id: @comment.author_id) if @comment.save
   end
+
   def destroy
     @comment = Comment.find(params[:id])
     authorize! :destroy, @comment
@@ -19,7 +20,9 @@ class CommentsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
   private
+
   def comment_params
     params.require(:comment).permit(:text, :author_id, :post_id)
   end
